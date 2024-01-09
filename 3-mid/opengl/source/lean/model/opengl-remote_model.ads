f4baa64e8f5b804626b91d4db474aa3fd68c824c
Rod Kay <rodakay5@gmail.com>
2024-01-09 16:57:56 +1100

    opengl.remote_model: Set default 'Shine' to '1.0'.

--- 3-mid/opengl/source/lean/model/opengl-remote_model.ads
+++ 3-mid/opengl/source/lean/model/opengl-remote_model.ads
@@ -7,7 +7,7 @@ is
    type Item is abstract tagged
       record
          Id    : model_Id     := null_model_Id;
-         Shine : openGL.Shine := 200.0;
+         Shine : openGL.Shine := 1.0;
       end record;
 
 end openGL.remote_Model;
