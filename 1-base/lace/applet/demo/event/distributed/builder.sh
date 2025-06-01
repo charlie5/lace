9dcac72ad5203111f96d3384c522a27e45442fa5
Rod Kay <rodakay5@gmail.com>
2025-06-01 14:39:47 +1000

    lace.demo.event.distributed: Add gprclean to 'builder.sh' and update 'polyorb.confs' in 'lan' test.

--- 1-base/lace/applet/demo/event/distributed/builder.sh
+++ 1-base/lace/applet/demo/event/distributed/builder.sh
@@ -14,6 +14,7 @@ cp /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/adalib/a-sttebu.ali \
     dsa/x86_64-unknown-linux-gnu/obj
 
 export Build_Mode=debug
+gprclean    -r simple_chat.gpr
 po_gnatdist -P simple_chat.gpr simple_chat.dsa -cargs -g -largs -g
 
 #rm -fr build
