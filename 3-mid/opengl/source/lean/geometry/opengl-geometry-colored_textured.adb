e302518c814492f37ee6f17e0feed84cd636062d
Rod Kay <rodakay5@gmail.com>
2025-09-17 08:57:27 +1000

    opengl.geometry.colored_textured: Use texturing shader snippet.

--- 3-mid/opengl/source/lean/geometry/opengl-geometry-colored_textured.adb
+++ 3-mid/opengl/source/lean/geometry/opengl-geometry-colored_textured.adb
@@ -78,7 +78,9 @@ is
             white_Texture := openGL.Texture.Forge.to_Texture (white_Image);
 
             vertex_Shader  .define (Shader.Vertex,   "assets/opengl/shader/colored_textured.vert");
-            fragment_Shader.define (Shader.Fragment, "assets/opengl/shader/colored_textured.frag");
+            fragment_Shader.define (Shader.Fragment, (asset_Names' (1 => to_Asset ("assets/opengl/shader/version.header"),
+                                                                    2 => to_Asset ("assets/opengl/shader/texturing-frag.snippet"),
+                                                                    3 => to_Asset ("assets/opengl/shader/colored_textured.frag"))));
 
             the_Program := new openGL.Program.item;
             the_Program.define (vertex_Shader  'Access,
