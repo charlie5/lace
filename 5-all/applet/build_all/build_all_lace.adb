564897e3c17fe92db6b52309420ed426e4e5983e
Rod Kay <rodakay5@gmail.com>
2026-08-20 23:37:59 +1000

    Apply formatting rules to all 'build_all_Lace' source code.

--- 5-all/applet/build_all/build_all_lace.adb
+++ 5-all/applet/build_all/build_all_lace.adb
@@ -1,8 +1,8 @@
 with
 --     launch_simple_chat_Client,
 --     launch_simple_chat_Registrar,
-     launch_simple_instant_events_demo,
-     launch_simple_deferred_events_demo,
+     launch_simple_instant_events_Demo,
+     launch_simple_deferred_events_Demo,
 
      launch_strings_Demo,
 
@@ -26,17 +26,17 @@ with
      launch_parse_Box,
 --       launch_learn_Linear,
 
-     launch_camera_Demo,
+     launch_Camera_Demo,
      launch_core_Test,
-     launch_large_terrain_Demo,
-     launch_many_boxes_Demo,
+     launch_large_Terrain_Demo,
+     launch_many_Boxes_Demo,
      launch_many_Boxes_textured_x1_Demo,
      launch_diffuse_Light,
      launch_render_Lighting,
      launch_Model_scaling,
      launch_render_Arrows,
      launch_render_Asteroids,
-     launch_render_billboards,
+     launch_render_Billboards,
      launch_render_Boxes,
      launch_render_Capsules,
      launch_render_Hex_Grid,
@@ -44,10 +44,10 @@ with
      launch_render_Screenshot,
      launch_render_Text,
      launch_tiling_Demo,
-     launch_two_cameras_Demo,
+     launch_two_Cameras_Demo,
      launch_freetype_linkage_Test,
-     --  launch_egl_linkage_Test,
-     --  launch_gl_linkage_Test,
+     -- launch_egl_linkage_Test,
+     -- launch_gl_linkage_Test,
 
      launch_hello_physics_interface_2D_Demo,
      launch_hello_physics_interface_3D_Demo,
@@ -58,16 +58,16 @@ with
      launch_GEL_fused,
      launch_full_Demo,
      launch_Pong,
-     launch_Hello_gel,
+     launch_hello_GEL,
      launch_opengl_Model,
-     launch_Mouse_motion,
-     launch_Mouse_selection,
+     launch_mouse_Motion,
+     launch_mouse_Selection,
      launch_mouse_Selection_2D,
      launch_human_Model,
      launch_human_Model_v1,
      launch_simple_Animation,
      launch_box_rig_1_bone_Demo,
-     launch_Rig_Demo,
+     launch_rig_Demo,
      launch_human_rig_Demo,
      launch_Chains_2d,
      launch_drop_Ball_on_Box,
@@ -77,10 +77,10 @@ with
      launch_mixed_Joints,
      launch_mixed_Joints_2d,
      launch_mixed_Shapes,
-     launch_text_Sprite_Demo,
+     launch_text_sprite_Demo,
      launch_add_rid,
-     launch_add_rid_Sprite_Test,
-     launch_pong_Tute;
+     launch_add_rid_sprite_Test,
+     launch_Pong_Tute;
 
 procedure build_all_Lace
 --
@@ -88,8 +88,8 @@ procedure build_all_Lace
 --
 is
 begin
---   launch_simple_chat_Client;
---   launch_simple_chat_Registrar;
+-- launch_simple_chat_Client;
+-- launch_simple_chat_Registrar;
    launch_simple_deferred_events_Demo;
    launch_simple_instant_events_Demo;
 
@@ -115,17 +115,17 @@ begin
    launch_parse_Box;
 --     launch_learn_Linear;
 
-   launch_camera_Demo;
+   launch_Camera_Demo;
    launch_core_Test;
-   launch_large_terrain_Demo;
-   launch_many_boxes_Demo;
+   launch_large_Terrain_Demo;
+   launch_many_Boxes_Demo;
    launch_many_Boxes_textured_x1_Demo;
    launch_diffuse_Light;
    launch_render_Lighting;
    launch_Model_scaling;
    launch_render_Arrows;
    launch_render_Asteroids;
-   launch_render_billboards;
+   launch_render_Billboards;
    launch_render_Boxes;
    launch_render_Capsules;
    launch_render_Hex_Grid;
@@ -133,30 +133,30 @@ begin
    launch_render_Screenshot;
    launch_render_Text;
    launch_tiling_Demo;
-   launch_two_cameras_Demo;
-   --  launch_egl_linkage_Test;
+   launch_two_Cameras_Demo;
+   -- launch_egl_linkage_Test;
    launch_freetype_linkage_Test;
-   --  launch_gl_linkage_Test;
+   -- launch_gl_linkage_Test;
 
    launch_hello_physics_interface_2D_Demo;
    launch_hello_physics_interface_3D_Demo;
-   launch_Test_Engine;
+   launch_test_Engine;
 
---   launch_Client;
---   launch_Server;
+-- launch_Client;
+-- launch_Server;
    launch_GEL_fused;
    launch_full_Demo;
    launch_Pong;
-   launch_Hello_gel;
+   launch_hello_GEL;
    launch_opengl_Model;
-   launch_Mouse_motion;
-   launch_Mouse_selection;
+   launch_mouse_Motion;
+   launch_mouse_Selection;
    launch_mouse_Selection_2D;
    launch_human_Model;
    launch_human_Model_v1;
    launch_simple_Animation;
    launch_box_rig_1_bone_Demo;
-   launch_Rig_Demo;
+   launch_rig_Demo;
    launch_human_rig_Demo;
    launch_Chains_2d;
    launch_drop_Ball_on_Box;
@@ -166,9 +166,9 @@ begin
    launch_mixed_Joints;
    launch_mixed_Joints_2d;
    launch_mixed_Shapes;
-   launch_text_Sprite_Demo;
+   launch_text_sprite_Demo;
    launch_add_rid;
-   launch_add_rid_Sprite_Test;
-   launch_pong_Tute;
+   launch_add_rid_sprite_Test;
+   launch_Pong_Tute;
 
 end build_all_Lace;
